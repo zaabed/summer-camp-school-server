2e36c7cf12436b99ee1acddef98422aa2c64a876
zaabed <zaaabedkhaan@gmail.com>
2023-08-27 04:03:42 +0600

    get courses

--- index.js
+++ index.js
@@ -72,6 +72,19 @@ async function run() {
 
         //Cart Collection-------------------------------------------------------------
 
+        //only show login user Data: 
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            if (!email) {
+                res.send([]);
+            }
+            else {
+                const query = { email: email };
+                const result = await cartCollection.find(query).toArray();
+                res.send(result);
+            }
+        })
+
         app.post('/carts', async (req, res) => {
             const item = req.body;
             const result = await cartCollection.insertOne(item);
