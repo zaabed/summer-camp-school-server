24615d58259f78bec82af118781ee186a0ced5a7
zaabed <zaaabedkhaan@gmail.com>
2023-09-03 23:07:58 +0600

    make role change

--- index.js
+++ index.js
@@ -62,6 +62,17 @@ async function run() {
 
         //Get Users who create account and use this website and save data on database----------------------------------------------------------------
 
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        })
+
+        //find admin Info
+        // app.get('/users/admin/', async (req, res) => {
+        //     const result = await usersCollection.find().toArray();
+        //     res.send(result);
+        // })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const query = { email: user.email };
@@ -72,6 +83,27 @@ async function run() {
             const result = await usersCollection.insertOne(user);
             res.send(result);
         })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
 
         //Get Courses And Teacher----------------------------------------------------
 
