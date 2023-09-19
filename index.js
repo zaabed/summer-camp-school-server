77b5714d9b224b173dc6b5f30f33b3eb49a2c6d0
zaabed <zaaabedkhaan@gmail.com>
2023-09-20 01:33:43 +0600

    load data

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const usersCollection = client.db('summerCamp').collection('users');
         // const classesCollection = client.db('summerCamp').collection('classes');
